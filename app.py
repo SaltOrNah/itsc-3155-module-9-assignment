8cc93a660852711a00b2e3b405a0cf379daebc3c
Chrisdouce <chrisdouce33321@gmail.com>
2024-03-15 14:40:07 -0400

    Creating basis for search movie feature

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, render_template
+from flask import Flask, redirect, render_template, request
 
 from src.repositories.movie_repository import get_movie_repository
 
@@ -34,7 +34,10 @@ def create_movie():
 @app.get('/movies/search')
 def search_movies():
     # TODO: Feature 3
-    return render_template('search_movies.html', search_active=True)
+    title = request.args.get('title')
+    #movie_repository.create_movie("The", "Tim", 5)
+    movie = movie_repository.get_movie_by_title(title)
+    return render_template('search_movies.html', search_active=True, movie=movie)
 
 
 @app.get('/movies/<int:movie_id>')
