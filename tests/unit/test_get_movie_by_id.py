db299cd99c677e84efaf482fc607d06163b8e8ed
gulpiegator <tristin.moore@rocketmail.com>
2024-03-19 17:26:14 -0400

    html, flask, and unit test finished

--- tests/unit/test_get_movie_by_id.py
+++ tests/unit/test_get_movie_by_id.py
@@ -1,1 +1,7 @@
 # TODO: Feature 4
+from src.repositories.movie_repository import get_movie_repository
+def test_get_movie_by_id():
+    repo = get_movie_repository()
+    movie = repo.create_movie('Star Wars', 'George Lucas', 5)
+    id = movie.movie_id
+    assert movie == repo.get_movie_by_id(id)
