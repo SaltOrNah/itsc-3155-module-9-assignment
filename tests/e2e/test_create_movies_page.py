5c41f5cde47913293d3971d58ca71a26ad73f12b
Nate Presson <npresson@uncc.edu>
2024-03-19 20:13:06 -0400

    Adding the movie form

--- tests/e2e/test_create_movies_page.py
+++ tests/e2e/test_create_movies_page.py
@@ -1,1 +1,11 @@
 # TODO: Feature 2
+from flask.testing import FlaskClient
+from app import app
+
+
+def test_home_page(test_app: FlaskClient):
+    response = test_app.get('/')
+    response_data = response.data.decode('utf-8')
+    assert '<h1 class="mb-5">Movie Rating App</h1>' in response_data
+    assert response.status_code == 200
+
--- tests/unit/test_create_movie.py
+++ tests/unit/test_create_movie.py
@@ -1,1 +1,16 @@
 # TODO: Feature 2
+from app import app
+import unittest
+from src.repositories.movie_repository import get_movie_repository
+
+def test_create_movie():
+    test_app = app.test_client()
+    response = test_app.get('/')
+    assert response.status_code == 200
+    test_repository = get_movie_repository()
+    test_repository.create_movie('Narnia', 'CS Lewis', 5)
+    assert test_repository.get_movie_by_title('Narnia').title == 'Narnia'
+    assert test_repository.get_movie_by_title('Narnia').director == 'CS Lewis'
+    assert type(test_repository.get_movie_by_title('Narnia').rating) is int
+    assert type (test_repository.get_movie_by_title('Narnia').movie_id) is int 
+
